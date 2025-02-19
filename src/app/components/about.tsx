e4750f07557ae35ce6f1bebfaf2c2dd8af967893
Gabe Morris <gabemorris1042@gmail.com>
2025-02-19 14:16:23 -0400

    various changes to about section images and p tags

--- src/app/components/about.tsx
+++ src/app/components/about.tsx
@@ -1,4 +1,6 @@
 
+import Image from "next/image";
+
 export default function About() {
   return (
     <section id="about" className="py-16 pt-[8rem] bg-[#dddddd] text-black">
@@ -9,22 +11,48 @@ export default function About() {
           discover the deeper meaning of the stars and how they influence your daily life. Whether you’re
           looking for daily horoscopes, zodiac sign compatibility, or astrological insights, we are here to illuminate your path.
         </p>
-        <div className="flex justify-center gap-6 mt-6">
-          <div className="w-64 h-64 bg-gray-800 rounded-lg flex items-center justify-center p-6"
 
-          >
-            <p className="text-gray-400">Astrology Readings</p>
+        <div className="flex justify-center gap-6 mt-6">
+
+          {/* Astrology Readings */}
+          <div className="w-64">
+            <div className="w-64 h-64 relative rounded-lg overflow-hidden hover:scale-110 cursor-pointer duration-300">
+              <Image
+                src="/assets/img/about/astrology-readings.webp"
+                alt="Astrology Readings"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <p className="text-black text-center pt-[1.5rem]">Astrology Readings</p>
           </div>
-          <div className="w-64 h-64 bg-gray-800 rounded-lg flex items-center justify-center p-6"
-          
-          >
-            <p className="text-gray-400">Zodiac Insights</p>
+
+          {/* Zodiac Insights */}
+          <div className="w-64">
+            <div className="w-64 h-64 relative rounded-lg overflow-hidden hover:scale-110 cursor-pointer duration-300">
+              <Image
+                src="/assets/img/about/zodiac-insights.webp"
+                alt="Astrology Readings"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <p className="text-black text-center pt-[1.5rem]">Zodiac Insights</p>
           </div>
-          <div className="w-64 h-64 bg-gray-800 rounded-lg flex items-center justify-center p-6"
-          
-          >
-            <p className="text-gray-400">Horoscope Predictions</p>
+
+          {/* Horoscope Predictions */}
+          <div className="w-64">
+            <div className="w-64 h-64 relative rounded-lg overflow-hidden hover:scale-110 cursor-pointer duration-300">
+              <Image
+                src="/assets/img/about/horoscope-readings.webp"
+                alt="Astrology Readings"
+                layout="fill"
+                objectFit="cover"
+              />
+            </div>
+            <p className="text-black text-center pt-[1.5rem]">Horoscope Predictions</p>
           </div>
+
         </div>
       </div>
     </section>
