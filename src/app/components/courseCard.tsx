cd5c4ccdf6babe250381b190dd200264246696e0
Gabe Morris <gabemorris1042@gmail.com>
2025-02-28 10:10:11 -0400

    framer motion scroll animations on CourseCards

--- src/app/components/courseCard.tsx
+++ src/app/components/courseCard.tsx
@@ -1,4 +1,7 @@
 
+"use client";
+import { motion } from "framer-motion";
+
 interface CardProps {
   title: string;
   items:string[];
@@ -6,15 +9,21 @@ interface CardProps {
 
 export default function CourseCard({ title, items }: CardProps) {
   return (
-    <div className="flex justify-center w-[80%] h-[100%] p-6 rounded-lg bg-[#FFFFFF] text-gray-900 transition-transform duration-300 ease-in-out hover:scale-110 shadow-lg">
-          <div className="flex flex-col items-center w-full">
-            <h2 className="font-bold pb-6 text-center text-4xl">{title}</h2>
-            <div className="flex flex-col items-center text-center font-semibold text-gray-600 text-lg space-y-3">
-              {items.map((item, index) => (
-                <p key={index}>{item}</p>
-              ))}
-            </div>
+      <motion.div 
+        className="flex justify-center w-[80%] h-[100%] p-6 rounded-lg bg-[#FFFFFF] text-gray-900"
+        whileHover={{ scale: 1.1 }}
+        whileTap={{ scale: 0.95 }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+      >
+        <div className="flex flex-col items-center w-full">
+          <h2 className="font-bold pb-6 text-center text-4xl">{title}</h2>
+          <div className="flex flex-col items-center text-center font-semibold text-gray-600 text-lg space-y-3">
+            {items.map((item, index) => (
+              <p key={index}>{item}</p>
+            ))}
           </div>
         </div>
+    </motion.div>
   )
 }
