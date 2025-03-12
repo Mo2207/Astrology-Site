1d066cf2f2250ef928b31eff88a39e0247e29a59
Gabe Morris <gabemorris1042@gmail.com>
2025-03-12 11:46:50 -0300

    header responsiveness and mobile menu functionality

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,7 +1,7 @@
 
 'use client'
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { useTranslation } from "react-i18next";
 import { ChevronDown } from "lucide-react";
 import { Button } from "./ui/button/button";
@@ -16,36 +16,68 @@ const languages = [
 
 export default function Header() {
   const { t, i18n } = useTranslation();
+  const [languagedDropdownOpen, setLanguagedDropdownOpen] = useState(false);
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
+  const mobileMenuRef = useRef<HTMLDivElement>(null);
+  const mobileBtnRef = useRef<HTMLButtonElement>(null);
 
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-
+  // language switcher
   const changeLanguage = (language: string) => {
     i18n.changeLanguage(language);
     localStorage.setItem("language", language);
-    setDropdownOpen(false);
+    setLanguagedDropdownOpen(false);
   }
-
   useEffect(() => {
     const savedLang = localStorage.getItem("language") || "en";
     i18n.changeLanguage(savedLang);
   }, []);
 
+  // mobile menu toggle
+  const handleToggleMenu = () => {
+    // setTimeout delays the state update until after the mousedown event is processed,
+    // this is to prevent the menu closing from the event and re-opening from this function
+    setTimeout(() => {
+      setMobileMenuOpen((prev) => !prev);
+    }, 1);
+  };
+  useEffect(() => {
+    // closes mobile menu when clicked outside
+    const handleClickOutside = (event: MouseEvent) => {
+      // check first that both exist and are rendered in the dom
+      if (!mobileMenuRef.current || !mobileBtnRef.current) return;
+
+      // close the menu when clicking outside
+      if (
+        !mobileMenuRef.current.contains(event.target as Node) &&
+        !mobileBtnRef.current.contains(event.target as Node)
+      ) {
+        setMobileMenuOpen(false);
+      }
+    };
+    // add listener on component mount
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      // clean up listener after
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+  },[])
+
   return (
     <header className="fixed top-0 left-0 w-full bg-[#faf5f3] text-gray-900 py-4 shadow-md z-50">
-      <div className="max-w-7xl mx-auto flex justify-between items-center px-6">
+      <div className=" mx-auto flex justify-between items-center px-4">
         
         {/* Logo */}
-        <div className="flex items-center gap-2">
+        <div className="flex items-center gap-2 px-3 mt-2">
           <Image
             src={logo}
             alt="main logo"
-            width={200}
-            height={200}
+            width={150}
+            height={150}
           />
         </div>
 
         {/* Navigation */}
-        <nav className="absolute left-1/2 transform -translate-x-1/2 hidden md:flex gap-6">
+        <nav className="hidden md:flex gap-6 ">
           {navs.map((nav) => (
             <a key={nav.id} onClick={() => handleScroll(nav.href)} className="hover:text-gray-400 hover:cursor-pointer transition">
               {t(`navs.${nav.id}.name`)}
@@ -58,22 +90,22 @@ export default function Header() {
 
         {/* Language Button */}
         <Button
-          className="flex items-center"
-          onClick={() => setDropdownOpen(!dropdownOpen)}
+          className="hidden md:flex items-center"
+          onClick={() => setLanguagedDropdownOpen(!languagedDropdownOpen)}
         >
           {languages.find((lang) => lang.code === i18n.language)?.label}
           <ChevronDown className="w-5 h-5" />
         </Button>
 
-        {/* Dropdown Menu */}
-        {dropdownOpen && (
+        {/* Language Dropdown Menu */}
+        {languagedDropdownOpen && (
           <div className="absolute right-0 mt-2 w-40 bg-[#faf5f3] rounded-lg shadow-lg">
             {languages.map((lang) => (
               <button
                 key={lang.code}
                 onClick={() => {
                   changeLanguage(lang.code);
-                  setDropdownOpen(false);
+                  setLanguagedDropdownOpen(false);
                 }}
                 className="block w-full px-4 py-2 text-left hover:bg-[#ad9585] hover:rounded-lg"
               >
@@ -82,15 +114,53 @@ export default function Header() {
             ))}
           </div>
         )}
-
         </div>
 
-        {/* Mobile Menu Button */}
+        {/* mobile menu hamburger  */}
         <div className="md:hidden">
-          <button className="text-white hover:text-yellow-400">
+          <button 
+            className="text-[#ddbea9] hover:text-[#ad9585] text-3xl"
+            onClick={handleToggleMenu} // toggle menu again when button clicked
+            ref={mobileBtnRef}
+          >
             ☰
           </button>
         </div>
+
+        {/* mobile menu dropdown */}
+        {mobileMenuOpen && (
+          <div 
+            className="absolute top-[5rem] right-[1rem] p-3 border-0 rounded-xl bg-[#faf5f3]"
+            ref={mobileMenuRef}
+          >
+            <div className="flex flex-col justify-center gap-4">
+              {navs.map((nav) => (
+                <button 
+                  key={nav.id}
+                  onClick={() => handleScroll(nav.href)} 
+                  className="hover:text-[#AD9585] hover:cursor-pointer transition flex flex-col py-2"
+                >
+                  {t(`navs.${nav.id}.name`)}
+                </button>
+              ))}
+            </div>
+
+            {/* language button toggle */}
+            <Button
+              className="items-center mt-4"
+              onClick={() => {
+                const nextLanguage = i18n.language === "en"? "ru" : "en";
+                changeLanguage(nextLanguage);
+              }}
+            >
+              {/* display opposite label */}
+              {languages.find((lang) => lang.code === (i18n.language === "en" ? "ru" : "en"))?.label}
+            </Button>
+
+          </div>
+        )}
+
+
       </div>
     </header>
   );
