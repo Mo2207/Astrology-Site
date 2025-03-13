5153b46a53bd91c1864773e74deb7b40a59d2548
Gabe Morris <gabemorris1042@gmail.com>
2025-03-13 13:34:22 -0300

    added decals back to gaurantee section

--- src/app/components/guarantee.tsx
+++ src/app/components/guarantee.tsx
@@ -9,29 +9,29 @@ export default function Guarantee() {
   const { t } = useTranslation();
 
   return (
-    <section id="price" className="pt-20 bg-white text-gray-900 flex justify-center">
+    <section id="price" className="pt-20 bg-white text-gray-900 flex justify-center relative">
       
-      {/* <Image
+      <Image
         src={graf}
         width={300}
         height={100}
         alt="spiral decal"
-        className="object-cover h-full px-8 self-center hidden"
-      /> */}
+        className="absolute left-2 z-0 hidden md:flex object-cover h-full self-center "
+      />
 
-      <div className="flex flex-col w-[80%] border border-[#DDBEA9] p-6 rounded-xl bg-[#faf5f3] text-center">
+      <div className="flex z-10 flex-col w-[80%] border border-[#DDBEA9] p-6 rounded-xl bg-[#faf5f3] text-center">
         <h2 className="self-center font-bold text-3xl pb-8">{t("guarantee.title")}</h2>
         <p className="text-lg pb-5">{t("guarantee.p1")}</p>
         <p className="text-lg">{t("guarantee.p2")}</p>
       </div>
 
-      {/* <Image
+      <Image
         src={graf}
         width={300}
         height={100}
         alt="spiral decal"
-        className="object-cover h-full px-8 self-center hidden"
-      /> */}
+        className="absolute md:right-2 z-0 hidden md:flex object-cover h-full self-center"
+      />
 
     </section>
   )
