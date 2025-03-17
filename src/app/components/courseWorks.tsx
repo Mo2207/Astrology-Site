b2051e129fcf525e95e62ef8cb8b1a4086a1c211
Gabe Morris <gabemorris1042@gmail.com>
2025-03-17 15:46:23 -0300

    more resizing for course works section

--- src/app/components/courseWorks.tsx
+++ src/app/components/courseWorks.tsx
@@ -11,33 +11,33 @@ export default function CourseWorks() {
   const cards = t("works.list", { returnObjects: true }) as { title: string; p: string; image: string; }[];
 
   return (
-    <section id="course_works" className="pt-12 pb-20 bg-[#FFFFFF] text-gray-900">
+    <section id="course_works" className="flex justify-center pt-12 pb-20 bg-[#FFFFFF] text-gray-900">
+      <div className="max-w-7xl self-center">
       <div className="flex justify-center text-center text-3xl font-bold pb-16 px-5">
         <h2>{t("works.title")}</h2>
       </div>
 
-      <div className="flex flex-col md:flex-row justify-center px-10 gap-5 items-stretch">
+      <div className="flex flex-col lg:flex-row justify-center px-10 gap-5 items-stretch">
 
         {/* LEFT SIDE */}
-        <div className="flex bg-[#FAF5F3] w-full md:w-2/5">
+        <div className="flex flex-1 w-full max-h-[500px] lg:max-h-none justify-center">
           <Image
             src={"/assets/img/works/flower-girl.jpeg"}
-            width={500}
-            height={500}
+            width={518}
+            height={778}
             alt="girl with flowers"
-            className="border-0 rounded-lg object-cover w-full max-h-[60vh] md:max-h-full"
-            style={{ objectPosition: "center top" }}
+            className="border-0 rounded-lg object-cover object-top max-h-full h-auto w-full"
           />
         </div>
 
         {/* RIGHT SIDE */}
-        <div className="flex flex-col w-full md:w-3/5">
+        <div className="flex flex-col w-full lg:w-[65%]">
 
           {/* cards */}
-          <div className="flex flex-col justify-between flex-grow">
+          <div className="flex flex-col gap-3">
             {cards.map((item, index) => (
               <div
-                className="flex flex-row border-0 rounded-xl p-5 bg-[#faf5f3] mb-2"
+                className="flex flex-row border-0 rounded-xl p-5 bg-[#faf5f3]"
                 key={index}
               >
                 <Image
@@ -56,7 +56,7 @@ export default function CourseWorks() {
 
             {/* pricing */}
             <div className="">
-              <div className="flex flex-row items-start gap-5">
+              <div className="flex flex-row justify-center lg:justify-start gap-5">
                 <CheckoutButton className="" text={t("buttons.gain_access")}></CheckoutButton>
                 <p className="mt-2 text-lg">
                   <span className="font-bold text-[#54994f]">{t("price.sale_price")}</span>
@@ -68,6 +68,7 @@ export default function CourseWorks() {
           </div>
         </div>
         
+      </div>
       </div>
     </section>
   )
