bd103be216f0499e851df081bf3e4800c3d8987c
Gabe Morris <gabemorris1042@gmail.com>
2025-03-20 10:22:47 -0300

    added left/right buttons for scrolling to curriculum and reviews sections on <medium screens

--- src/app/components/courseCurriculum.tsx
+++ src/app/components/courseCurriculum.tsx
@@ -2,11 +2,10 @@
 "use client";
 
 import { useTranslation } from "react-i18next";
-import { useRef, useState, useEffect } from "react";
+import { useRef } from "react";
 import flower from "../../../public/assets/img/curriculum/icons/flower.svg";
-import dots from "../../../public/assets/img/curriculum/icons/scrolling-dots.svg";
-// import left from "../../../public/assets/img/icons/arrow-left.svg";
-// import right from "../../../public/assets/img/icons/arrow-right.svg";
+import left from "../../../public/assets/img/icons/arrow-left.svg";
+import right from "../../../public/assets/img/icons/arrow-right.svg";
 import Image from "next/image";
 
 export default function CourseCurriculum() {
@@ -32,20 +31,20 @@ export default function CourseCurriculum() {
 
       <h2 className="text-2xl md:text-3xl lg-text-4xl font-bold text-center pb-10">{t("curriculum.title")}</h2>
 
-      <div className="relative w-full overflow-hidden justify-center flex">
+      <div className="relative w-full overflow-hidden justify-center flex h-[20rem]">
         
-      {/* <Image
+      <Image
           onClick={scrollLeft}
           src={left}
           alt="left arrow icon"
-          className="w-[3rem] h-[3rem] absolute left-5 bottom-0 -translate-x-y-1/2 sm:hidden hover:scale-150 transition delay-100 duration-300 ease-in-out"
+          className="w-[3rem] h-[3rem] absolute left-5 bottom-0 -translate-x-y-1/2 lg:hidden hover:scale-150 transition delay-100 duration-300 ease-in-out"
         />
         <Image
           onClick={scrollRight}
           src={right}
           alt="right arrow icon"
-          className="w-[3rem] h-[3rem] absolute right-5 bottom-0 -translate-x-y-1/2 sm:hidden hover:scale-150 transition delay-100 duration-300 ease-in-out"
-        /> */}
+          className="w-[3rem] h-[3rem] absolute right-5 bottom-0 -translate-x-y-1/2 lg:hidden hover:scale-150 transition delay-100 duration-300 ease-in-out"
+        />
 
         <div 
           className="flex flex-row space-x-6  px-6 pt-8 snap-x snap-mandatory  overflow-x-auto h-[16rem] w-full"
@@ -70,16 +69,7 @@ export default function CourseCurriculum() {
           ))}
         </div>
       </div>
-
-      <div className="flex justify-center">
-        <Image
-          src={dots}
-          alt="dots icon"
-          className="w-[3rem] h-[3rem]"
-        />
-      </div>
-      
-
+    
     </section>
   )
 }
--- src/app/components/reviews.tsx
+++ src/app/components/reviews.tsx
@@ -2,22 +2,43 @@
 "use client";
 
 import { useTranslation } from "react-i18next";
+import left from "../../../public/assets/img/icons/arrow-left.svg";
+import right from "../../../public/assets/img/icons/arrow-right.svg";
+import Image from "next/image";
+import { useRef } from "react";
 
 export default function Reviews() {
   const { t } = useTranslation();
 
   const reviews = t("reviews.list", { returnObjects: true }) as Record<string, { name: string; age: string; review: string }>;
 
+  const scrollContainerRef = useRef<HTMLDivElement>(null);
+
+  const scrollLeft = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: -300, behavior: "smooth" });
+    }
+  };
+
+  const scrollRight = () => {
+    if (scrollContainerRef.current) {
+      scrollContainerRef.current.scrollBy({ left: 300, behavior: "smooth" });
+    }
+  };
+
   return (
     <section id="reviews" className="bg-[#FFFFFF] text-gray-900 h-auto">
-      <h2 className="text-2xl md:text-3xl lg-text-4xl font-bold text-center">{t("reviews.title")}</h2>
+      <h2 className="text-2xl md:text-3xl lg-text-4xl font-bold text-center pb-10">{t("reviews.title")}</h2>
 
-      <div className="relative w-full overflow-hidden snap-start">
-        <div className="flex flex-row items-start space-y-0 space-x-6 px-6 pt-8 snap-x snap-mandatory scrollbar-hide overflow-y-hidden w-full">
+      <div className="relative w-full overflow-hidden snap-start pb-10 h-[500px]">
+        <div 
+          className="flex flex-row items-start space-y-0 space-x-6 px-6 pt-8 snap-x snap-mandatory scrollbar-hidden overflow-y-hidden w-full h-full md:h-[30rem]"
+          ref={scrollContainerRef}
+        >
           {Object.keys(reviews).map((key) => (
             <div
               key={key}
-              className="flex-shrink-0 flex flex-col w-full md:h-[25rem] md:w-[30rem] min-w-0 bg-[#faf5f3] shadow-md rounded-xl p-6 snap-center border border-[#AD6049]"
+              className="flex-shrink-0 flex flex-col w-full h-[90%] md:w-[30rem] min-w-0 bg-[#faf5f3] shadow-md rounded-xl p-6 snap-center border border-[#AD6049] overflow-y-auto"
             >
               <h3 className="text-xl font-semibold">{reviews[key].name}</h3>
               <h2 className="font-light pb-3">{reviews[key].age}</h2>
@@ -25,7 +46,23 @@ export default function Reviews() {
             </div>
           ))}
         </div>
-      </div>
+
+
+          <Image
+            onClick={scrollLeft}
+            src={left}
+            alt="right arrow icon"
+            className="w-[3rem] h-[3rem] absolute left-5 bottom-0 -translate-x-y-1/2  hover:scale-150 transition delay-100 duration-300 ease-in-out lg:hidden"
+          />
+          <Image
+            onClick={scrollRight}
+            src={right}
+            alt="right arrow icon"
+            className="w-[3rem] h-[3rem] absolute right-5 bottom-0 -translate-x-y-1/2  hover:scale-150 transition delay-100 duration-300 ease-in-out lg:hidden"
+          />
+        </div>
+      
+
     </section>
   )
 }
