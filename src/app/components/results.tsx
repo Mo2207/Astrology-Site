1ad3a6a982e2139dc1d3a5efe1eb18d93240bd44
Gabe Morris <gabemorris1042@gmail.com>
2025-03-20 12:03:47 -0300

    h2 below results with translation

--- src/app/components/results.tsx
+++ src/app/components/results.tsx
@@ -9,14 +9,14 @@ import check from "../../../public/assets/img/icons/check.svg";
 export default function Results() {
   const { t } = useTranslation();
 
-  const results = t("results.list", {returnObjects: true}) as string[];
+  const results = t("results.list", { returnObjects: true }) as string[];
 
   return (
     <section id="results" className="bg-[#faf5f3] text-gray-900 h-auto py-12 flex items-center justify-center">
-      <div className="max-w-7xl flex justify-center">
+      <div className="max-w-7xl flex justify-center felx flex-col">
 
-      <div className="flex items-center flex-col w-[90%]">
-        <h1 className="font-bold text-2xl md:text-3xl text-center p-5">{t("results.title")}</h1>
+        <div className="flex items-center flex-col w-[90%]">
+          <h1 className="font-bold text-2xl md:text-3xl text-center p-5">{t("results.title")}</h1>
           <div className="p-5 text-md md:text-lg">
             {results.map((item, index) => (
               <p
@@ -32,7 +32,10 @@ export default function Results() {
               </p>
             ))}
           </div>
-      </div>
+        </div>
+
+        <h2 className="text-lg md:text-xl font-bold p-6 self-center text-center">{t("results.h2")}</h2>
+
       </div>
     </section>
   )
