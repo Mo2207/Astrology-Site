a5a77d672298819c46adc90a89c308a954f09ca9
Gabe Morris <gabemorris1042@gmail.com>
2025-03-31 11:52:13 -0300

    replaced telegram link generator for static link

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -36,33 +36,35 @@ export async function POST(req: Request) {
       }
 
       // telegram/courseLink logic
-      const botToken = process.env.TELEGRAM_BOT_TOKEN!;
-      const channelId = '-1002548077845';
+      // const botToken = process.env.TELEGRAM_BOT_TOKEN!;
+      // const channelId = '-1002548077845';
 
-      const telegramResponse = await fetch(
-        `https://api.telegram.org/bot${botToken}/createChatInviteLink`,
-        {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify({
-            chat_id: channelId,
-            member_limit: 1,
-            creates_join_reques: false,
-          }),
-        }
-      );
+      // const telegramResponse = await fetch(
+      //   `https://api.telegram.org/bot${botToken}/createChatInviteLink`,
+      //   {
+      //     method: 'POST',
+      //     headers: { 'Content-Type': 'application/json' },
+      //     body: JSON.stringify({
+      //       chat_id: channelId,
+      //       member_limit: 1,
+      //       creates_join_reques: false,
+      //     }),
+      //   }
+      // );
 
-      // error for telegramResponse
-      if (!telegramResponse.ok) {
-        const errorText = await telegramResponse.text();
-        console.error("❌ Telegram API failed:", errorText);
-        return NextResponse.json({ error: "Failed to create Telegram invite link" }, { status: 500 });
-      }
+      // // error for telegramResponse
+      // if (!telegramResponse.ok) {
+      //   const errorText = await telegramResponse.text();
+      //   console.error("❌ Telegram API failed:", errorText);
+      //   return NextResponse.json({ error: "Failed to create Telegram invite link" }, { status: 500 });
+      // }
 
-      // parse the response
-      const telegramData = await telegramResponse.json();
-      const courseLink = telegramData.result.invite_link;
-      console.log("✅ Telegram invite link:", courseLink);
+      // // parse the response
+      // const telegramData = await telegramResponse.json();
+      // const courseLink = telegramData.result.invite_link;
+      // console.log("✅ Telegram invite link:", courseLink);
+
+      const courseLink = "https://t.me/Intensiv_pro_resourse_bot";
 
       // send the email via resend
       await resend.emails.send({
