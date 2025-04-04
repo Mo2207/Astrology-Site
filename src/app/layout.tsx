3d234b96ef47ff69c795d064f927d0476d390163
Gabe Morris <gabemorris1042@gmail.com>
2025-04-04 12:12:01 -0300

    script change in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,21 +34,31 @@ export default function RootLayout({ children,}: Readonly<{children: React.React
         className={`${geistSans.variable} ${geistMono.variable} antialiased overflow-x-hidden w-[100vw]`}
       >
 
-      {/* facebook pixel */}
+      {/* meta pixel code */}
       <Script id="facebook-pixel" strategy="afterInteractive">
-          {`
-            !function(f,b,e,v,n,t,s)
-            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-            n.queue=[];t=b.createElement(e);t.async=!0;
-            t.src=v;s=b.getElementsByTagName(e)[0];
-            s.parentNode.insertBefore(t,s)}(window, document,'script',
-            'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '663368119726030');
-            fbq('track', 'PageView');
-          `}
-        </Script>
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '663368119726030');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      
+      {/* Meta Pixel NoScript fallback */}
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src="https://www.facebook.com/tr?id=663368119726030&ev=PageView&noscript=1"
+        />
+      </noscript>
 
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-9V0PJQ81XL"
