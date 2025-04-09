a43969f74410ec28db9aaea313116719f7c9d6cb
Gabe Morris <gabemorris1042@gmail.com>
2025-04-09 09:33:00 -0300

    added suspense for page render

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,13 @@
 
-"use client"
-
-import Footer from "./components/footer";
-import Header from "./components/header";
-import About from "./components/about";
-import Contact from "./components/contact";
-import CourseCards from "./components/courseCards";
-import CourseCurriculum from "./components/courseCurriculum";
-import Bonus from "./components/bonus";
-import Hero from "./components/hero";
-import Reviews from "./components/reviews";
-import Results from "./components/results";
-import CourseDifference from "./components/courseDifference";
-import CourseIntro from "./components/courseIntro";
-import CourseWorks from "./components/courseWorks";
-import Price from "./components/price";
-import Guarantee from "./components/guarantee";
-import Faq from "./components/faq";
-import MobileHoverButton from "./components/mobileHoverButton";
+import { Suspense } from 'react';
+import ClientPage from './clientPage';
 
 export default function Page() {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <CourseCards />
-      <About />
-      <CourseCurriculum />
-      <CourseIntro />
-      <Bonus />
-      <Reviews />
-      <Guarantee />
-      <Results />
-      <Faq />
-      <Contact />
-      <CourseWorks />
-      <Price />
-      <CourseDifference />
-      <Footer />
-      <MobileHoverButton />
-    </div>
-  )
+    <Suspense fallback={<div>Loading...</div>}>
+      <ClientPage />
+    </Suspense>
+  );
 }
+
 
